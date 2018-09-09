c9df728c37a7693e831c2295307e506566f92e4a
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2018-09-09 13:59:44 +0200

    Fix travis build

--- generate_tests.py
+++ generate_tests.py
@@ -12,7 +12,7 @@ print("Running Notebooks...")
 os.chdir('notebooks')
 \n\n""")
 
-for i, nb in enumerate(filter(lambda f:  not "Errors" in f,nbfiles)):  
+for i, nb in enumerate(filter(lambda f:  not "Errors" in f,sorted(nbfiles))):  
     with open("tests.py", "a") as f:
         f.write(f"def test_ipynb_{i}():\n")
         f.write(f"\t nb, errors = _notebook_run('{nb[10:]}')\n")
